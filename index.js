a6e0f50dfb59d309d2a75e69b3557a393910c5f9
Miguel C <migs.castaneda@gmail.com>
2022-07-02 13:54:41 -0400

    more commits

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
-require('dotenv').config()
-const PORT = process.env.PORT
+
+
 
 app.use('/places', require('./controllers/places'))
 
@@ -10,10 +12,10 @@ app.get('/', (req,res) => {
     res.send('Hello world')
 })
 
-addEventListener.get ('*', (req,res) => {
+app.get ('*', (req,res) => {
     res.status(404).send('<h1>404 page</h1>')
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log("Let's get this Bread")
 })
