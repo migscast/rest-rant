b2411c4ee05fa85e8636d39ef768c448cd76e574
Miguel C <migs.castaneda@gmail.com>
2022-07-06 16:15:19 -0400

    adding error 404 and fixing home

--- views/home.jsx
+++ views/home.jsx
@@ -2,11 +2,13 @@ const React = require('react')
 const Def = require('./default')
 
 function home() {
-    <Def>
-        <main>
-            <h1>HOME</h1>
-        </main>
-    </Def>
+    return (
+        <Def>
+            <main>
+                <h1>HOME</h1>
+            </main>
+        </Def>
+    )
 }
 
 module.exports = home
