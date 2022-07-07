8c999f155791d42ff9921bfa791093e08748e2df
Miguel C <migs.castaneda@gmail.com>
2022-07-06 21:15:05 -0400

    adding bootstrap grid to index

--- controllers/places.js
+++ controllers/places.js
@@ -9,13 +9,13 @@ router.get('/', (req,res) => {
             city: 'Jersey City',
             state: 'NJ',
             cuisines: 'Bar Food',
-            pic: 'https://templatelab.com/wp-content/uploads/2017/04/puzzle-piece-template-13.jpg'
+            pic: '/images/puzzle-piece.jpg'
         },{
             name: 'Shokugeki no Restaurant',
             city: 'New York City',
             state: 'NY',
             cuisines: 'Anime Food',
-            pic: 'https://ih1.redbubble.net/image.1051566249.2427/tst,small,507x507-pad,600x600,f8f8f8.jpg'
+            pic: '/images/manga-meat.jpg'
         }]
         res.render('places/index', {places})
     
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -5,9 +5,15 @@ const Def = require('../default')
 function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
-        <div>
+        <div className="col-sm-6">
           <h2>{place.name}</h2>
-          <img src={place.pic} alt={place.name}></img>
+          <p className='text-center'>
+            {place.cuisines}
+          </p>
+          <img src={place.pic} alt={place.name}/>
+          <p className='text-center'>
+            Located in {place.city}, {place.state}
+          </p>
         </div>
       )
     })
