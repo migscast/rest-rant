533705c2913ab94a1a648ed1531091f459347dff
Miguel C <migs.castaneda@gmail.com>
2022-07-06 21:03:36 -0400

    images acquired

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home() {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/ramen.jpg" alt="Ramen"/>
+                    <div>
+                        Photo by <a href="https://wallpapercave.com/anime-food-wallpapers">Caveman</a> on <a href="https://wallpapercave.com/">WallpaperCave</a>
+                    </div>
+                </div>
                 <a href="/places">
                 <button className="btn-primary">Places Page</button>
                 </a>
