1596fbcff6b2fc8e391f4c3521f64bcc0ea7b555
Miguel C <migs.castaneda@gmail.com>
2022-07-09 11:17:44 -0400

    isuues deleting

--- controllers/places.js
+++ controllers/places.js
@@ -42,5 +42,19 @@ router.post('/', (req, res) => {
     res.redirect('/places')
   })
   
+  router.delete('/places/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+        place.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
+  
 
 module.exports = router
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('method'))
+app.use(methodOverride('_method'))
 
 // Controllers & Routes
 app.use('/places', require('./controllers/places'))
