81dbf2f8acff16e8671c4c5a9e8e2b058aa67308
Miguel C <migs.castaneda@gmail.com>
2022-07-13 21:44:02 -0400

    organizing controllers

--- controllers/places.js
+++ controllers/places.js
@@ -2,10 +2,11 @@ const router = require('express').Router()
 const places = require('../models/places.js')
 
 // More code here in a moment
+//NEW
 router.get('/new', (req, res) => {
     res.render(`places/new`)
   })
-  
+  //EDIT
   router.get('/:id/edit', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
@@ -18,7 +19,7 @@ router.get('/new', (req, res) => {
         res.render('places/edit', {place: places[id], id})
     }
   })  
-
+//SHOW
 router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
@@ -31,13 +32,13 @@ router.get('/:id', (req, res) => {
     res.render('places/show', { place: places[id], id } )
     }
   })
-
+//INDEX
   router.get('/', (req,res) => {
         res.render(`places/index`, {places})
 })
 
 
-
+//POST
 router.post('/', (req, res) => {
     console.log(req.body)
     if (!req.body.pic) {
@@ -53,7 +54,7 @@ router.post('/', (req, res) => {
     places.push(req.body)
     res.redirect('/places')
   })
-
+//UPDATE
   router.put('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
@@ -83,7 +84,7 @@ router.post('/', (req, res) => {
   
 
   
-  
+  //DELETE
   router.delete('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
