1939c657f3e8cb73a75ab7eb5c85dce4db395ffc
Miguel C <migs.castaneda@gmail.com>
2022-07-23 02:46:28 -0400

    having some issues

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,25 @@ function show (data) {
       No comments yet
     </h3>
   )
+  let rating = (
+    <h3 className='inactive'>
+      Not yet rated
+    </h3>
+  )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for(let i=0; i< averageRating; i++) {
+      stars += '★'
+    }
+    rating = (
+      <h3>
+      {stars} stars
+      </h3>
+    )
     comments = data.place.comments.map(c => {
       return (
         <div className='border'>
@@ -27,6 +45,7 @@ function show (data) {
             <h1>{data.place.name}</h1>
             <h3>Located in {data.place.city}, {data.place.state}</h3>
             <h2>Rating</h2>
+            {rating}
             <h2>Description</h2>
             <h3>{data.place.showEstablished()}</h3>
             <h4>serving {data.place.cuisines}</h4>
@@ -49,3 +68,4 @@ function show (data) {
 }
 
 module.exports = show
+
