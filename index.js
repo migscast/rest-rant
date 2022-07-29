e6b76bf00ab8c64af30f87e8dff1b7d7998c9e49
Miguel C <migs.castaneda@gmail.com>
2022-07-28 23:35:35 -0400

    trying something

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 // Controllers & Routes
 
 
